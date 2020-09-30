8212f367f2ff501331a1a4002d9f00ccf4e811f4
flashton2003 <flashton@gmail.com>
2020-09-30 12:11:18 +0200

    changing approach to tree conversion

--- kraken2_scripts/parse_kraken2_report.py
+++ kraken2_scripts/parse_kraken2_report.py
@@ -1,46 +1,49 @@
 import os
+import string
 # import pandas as pd
 from anytree import Node, RenderTree
 
 class Taxon():
     def __init__(self, split_line):
-        self.percent_reads_mapped = float(split_line[0])
+        self.percent_reads_assigned = float(split_line[0])
         self.number_reads_rooted_here = int(split_line[1])
         self.number_reads_assigned_here = int(split_line[2])
-        self.rank = split_line[3]
+        self.full_rank = split_line[3]
+        self.rank = self.full_rank.rstrip(string.digits)
         self.ncbi_id = int(split_line[4])
         self.name = split_line[5].strip()
-
-# def my_sum(arg):
-#     total = 0
-#     for val in arg:
-#         total += val
-#     return total
 
 def read_kraken_report(inhandle):
     kraken_report_taxa = []
     with open(inhandle) as fi:
         for line in fi.readlines():
             split_line = line.strip().split('\t')
-            print(split_line)
+            # print(split_line)
             taxon = Taxon(split_line)
             kraken_report_taxa.append(taxon)
     return kraken_report_taxa
 
-def convert_report_to_tree(kraken_report):
+def convert_report_taxa_to_tree(kraken_report, taxa_levels):
     '''
     1. go thorugh the report lines
     2. if the line has more than 0.05% of the reads, consider it for inclusion in the output
     3. only include it in the output if there isn't an entry below it in the taxonomic level which 
     '''
-    root = Node('root')
+    assert kraken_report[0].name == 'unclassified'
+    assert kraken_report[1].name == 'root'
+    root = Node('root', percent_reads_assigned = kraken_report[1].percent_reads_assigned)
+    current_level = 1
+    for taxon in kraken_report[3:]:
+        if taxa_levels.index(taxon.full_rank) > current_level:
+            Node(taxon.name, parent = root)
+
+
+
     '''
     current_level = 0
     for line in report:
         if line.level > current_level:
             node("Name", parent = current_level)
-
-
     '''
 
 
@@ -52,18 +55,20 @@ def parse_tree(kraken_tree):
     '''
     pass
 
-def main(inhandle):
+def main(inhandle, taxa_levels):
     '''
     1. read in kraken report
     2. parse the report
     '''
-    tax_levels = ['U', 'R', 'D', 'K', 'P', 'C', 'O', 'F', 'G', 'S']
+    
     kraken_report_taxa = read_kraken_report(inhandle)
+    convert_report_taxa_to_tree(kraken_report_taxa, taxa_levels)
     # parse_report(kraken_report)
 
 inhandle = '/Users/flashton/Dropbox/GordonGroup/ben_kumwenda_genomes/kraken2/results/2020.09.30/18080-1-FR10242277.kraken_report.txt'
+taxa_levels = ['U', 'R', 'R1', 'D', 'D1', 'K', 'P', 'C', 'O', 'F', 'G', 'G1', 'S']
 
 assert os.path.exists(inhandle)
 
 if __name__ == '__main__':
-    main(inhandle)
+    main(inhandle, taxa_levels)
--- tests/unit/test_parse_kraken2_report.py
+++ tests/unit/test_parse_kraken2_report.py
@@ -1,20 +1,25 @@
 import unittest
 
 
-from kraken2_scripts.parse_kraken2_report import my_sum, read_kraken_report
+from kraken2_scripts.parse_kraken2_report import Taxon, taxa_levels
 
-class TestSum(unittest.TestCase):
-    def test_list_int(self):
-        '''
-        Test that it can sum a list of integers
-        '''
-        data = [1, 2, 3]
-        result = my_sum(data)
-        self.assertEqual(result, 6)
 
-class TestTaxonClassInit(unittest.TestCase):
+class TestParseKraken2Report(unittest.TestCase):
     def test_taxon_class_init(self):
-        split_line = 
+        ## salmonella rank is not actually S1, it's just S, but added the S1 to test the rank/full rank switch
+        split_line = ['26.78', '759833', '759833', 'S1', '28901', '                Salmonella enterica']
+        taxa = Taxon(split_line)
+        self.assertEqual(taxa.percent_reads_assigned, 26.78, 'Taxon class did not read in percent_reads_assigned correctly') 
+        self.assertEqual(taxa.number_reads_rooted_here, 759833, 'Taxon class did not read in number_reads_rooted_here correctly')
+        self.assertEqual(taxa.number_reads_assigned_here, 759833, 'Taxon class did not read in number_reads_assigned_here correctly')
+        self.assertEqual(taxa.full_rank, 'S1', 'Taxon class did not read in rank correctly')
+        self.assertEqual(taxa.rank, 'S', 'Taxon class did not read in rank correctly')
+        self.assertEqual(taxa.ncbi_id, 28901, 'Taxon class did not read in ncbi_id correctly')
+        self.assertEqual(taxa.name, 'Salmonella enterica', 'Taxon class did not read in name correctly')
+
+    def test_taxa_levels(self):
+        self.assertEqual(taxa_levels, ['U', 'R', 'D', 'K', 'P', 'C', 'O', 'F', 'G', 'S'], 'Taxa levels corrupted')
+
 
 if __name__ == '__main__':
     unittest.main()
