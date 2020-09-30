847561bf56d0d9849a469076e7a25336291e833d
flashton2003 <flashton@gmail.com>
2020-09-30 19:42:04 +0200

    tree working

--- kraken2_scripts/parse_kraken2_report.py
+++ kraken2_scripts/parse_kraken2_report.py
@@ -1,9 +1,9 @@
 import os
 import string
-from anytree import Node, RenderTree
+from anytree import Node, RenderTree, PreOrderIter, Walker
 
 class Taxon():
-    def __init__(self, split_line, taxa_levels):
+    def __init__(self, split_line, taxa_levels, sample_name):
         self.percent_reads_assigned = float(split_line[0])
         self.number_reads_rooted_here = int(split_line[1])
         self.number_reads_assigned_here = int(split_line[2])
@@ -12,48 +12,20 @@ class Taxon():
         self.taxonomic_level = taxa_levels.index(self.full_rank)
         self.ncbi_id = int(split_line[4])
         self.name = split_line[5].strip()
+        self.sample_name = sample_name
+        self.printed_already = False
 
-class BinaryNode():
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-
-    def insert_left(self, child):
-        if self.left is None:
-            self.left = child
-        else:
-            child.left = self.left
-            self.left = child
-
-class Tree():
-    def __init__(self, val):
-        self.val = val
-        self.children = []
-
-    def insert_child(self, val):
-        self.children.append(Tree(val))
-
-    def __repr__(self):
-        return f'Tree({self.val}): {self.children}'
-        
-tree = Tree('a')
-tree.insert_child('b')
-tree.insert_child('c')
-
-# print(root.val)
-# for child in root.children:
-    # print(child.val)
-# print(tree)
-
+    def print_info(self):
+        print(self.sample_name, self.percent_reads_assigned, self.number_reads_rooted_here, self.number_reads_assigned_here, self.full_rank, self.ncbi_id, self.name, sep = '\t')
 
 def read_kraken_report(inhandle, taxa_levels):
+    sample_name = inhandle.split('/')[-1].split('.')[0]
     kraken_report_taxa = []
     with open(inhandle) as fi:
         for line in fi.readlines():
             split_line = line.strip().split('\t')
             # print(split_line)
-            taxon = Taxon(split_line, taxa_levels)
+            taxon = Taxon(split_line, taxa_levels, sample_name)
             kraken_report_taxa.append(taxon)
     return kraken_report_taxa
 
@@ -97,22 +69,17 @@ def add_parents_to_taxa(kraken_report, taxa_levels):
         ## as the last thing set the current taxon as the "previous level"
         ## for the next loop
         previous_taxonomic_level = taxon.taxonomic_level
-            
-        if hasattr(taxon, "parent"):
-            print(taxon.name, taxon.parent, sep = '\t')
+        # if hasattr(taxon, "parent"):
+            # print(taxon.name, taxon.parent, sep = '\t')
     return kraken_report
 
 def make_tree(kraken_report):
-    # root = Node('root', taxon = kraken_report[1])
     kraken_report[1].node = Node('root', taxon = kraken_report[1])
     for taxon in kraken_report[2:]:
-        taxon.node = Node(taxon.name, parent = taxon.parent.node)
-        print(vars(taxon))
-
-    print(RenderTree(kraken_report[1].node))
-
-
-
+        ## make the parent the node attached to the parent in the taxon class
+        taxon.node = Node(taxon.name, parent = taxon.parent.node, taxon = taxon)
+    return kraken_report[1].node
+    # print(RenderTree(kraken_report[1].node))
 
 def parse_tree(kraken_tree):
     '''
@@ -120,7 +87,31 @@ def parse_tree(kraken_tree):
     2. take path between each tip and root
     3. go up the path towards the root until get to 
     '''
-    pass
+    w = Walker()
+    # print(kraken_tree.root)
+    for node in PreOrderIter(kraken_tree):
+        # print(dir(node))
+        if node.is_leaf:
+            # print(node)
+            # print()
+            i = 0
+            ## only want two levels up from each tip
+            while i < 3:
+                ## walk between the leaf node and the root
+                ## output of walk is a tuple of tuples, hence the [0]
+                for node2 in w.walk(node, kraken_tree.root)[0]:
+                    ## only want to print species or genus level
+                    if node2.taxon.rank in ('S', 'G'):
+                        ## if more than 0.05 reads assigned
+                        if node2.taxon.percent_reads_assigned >= 0.05:
+                            ## prints double for genus as multiple tips lead to same genus
+                            if node2.taxon.printed_already == False:
+                                node2.taxon.print_info()
+                                node2.taxon.printed_already = True
+                    i += 1
+                # if node2.taxon
+
+    # pass
 
 def main(inhandle, taxa_levels):
     '''
@@ -129,10 +120,10 @@ def main(inhandle, taxa_levels):
     2. if the line has more than 0.05% of the reads, consider it for inclusion in the output
     3. only include it in the output if there isn't an entry below it in the taxonomic level which 
     '''
-    
     kraken_report_taxa = read_kraken_report(inhandle, taxa_levels)
     kraken_report = add_parents_to_taxa(kraken_report_taxa, taxa_levels)
-    make_tree(kraken_report)
+    kraken_tree = make_tree(kraken_report)
+    parse_tree(kraken_tree)
     # parse_report(kraken_report)
 
 inhandle = '/Users/flashton/Dropbox/GordonGroup/ben_kumwenda_genomes/kraken2/results/2020.09.30/18080-1-FR10242277.kraken_report.txt'
